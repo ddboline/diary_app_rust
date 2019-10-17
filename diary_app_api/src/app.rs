0150ad4ef35c7f6bbec577b8f5ec6db0028b70cd
Daniel Boline <dboline@mediamath.com>
2019-10-17 12:20:14 -0400

    start refactoring

--- diary_app_api/src/app.rs
+++ diary_app_api/src/app.rs
@@ -7,6 +7,8 @@ use futures::future::Future;
 use futures::stream::Stream;
 use std::time;
 use tokio_timer::Interval;
+use std::sync::Arc;
+use std::sync::atomic::AtomicUsize;
 
 use diary_app_lib::config::Config;
 use diary_app_lib::diary_app_interface::DiaryAppInterface;
@@ -20,6 +22,7 @@ use super::routes::{
 
 pub struct AppState {
     pub db: Addr<DiaryAppInterface>,
+    pub start: Arc<AtomicUsize>,
 }
 
 pub fn start_app() {
