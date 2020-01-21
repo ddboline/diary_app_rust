6fb0375d3afb08adf506aa7231910789f623fe6e
Daniel Boline <ddboline@gmail.com>
2020-01-20 22:28:18 -0500

    refactor config

--- diary_app_lib/src/config.rs
+++ diary_app_lib/src/config.rs
@@ -22,6 +22,36 @@ pub struct ConfigInner {
 #[derive(Default, Debug, Clone)]
 pub struct Config(Arc<ConfigInner>);
 
+macro_rules! set_config_parse {
+    ($s:ident, $id:ident) => {
+        $s.$id = var(&stringify!($id).to_uppercase())
+            .ok()
+            .and_then(|x| x.parse().ok());
+    };
+}
+
+macro_rules! set_config_parse_default {
+    ($s:ident, $id:ident, $d:expr) => {
+        $s.$id = var(&stringify!($id).to_uppercase())
+            .ok()
+            .and_then(|x| x.parse().ok())
+            .unwrap_or_else(|| $d);
+    };
+}
+
+macro_rules! set_config_must {
+    ($s:ident, $id:ident) => {
+        $s.$id = var(&stringify!($id).to_uppercase())
+            .map_err(|e| format_err!("{} must be set: {}", stringify!($id).to_uppercase(), e))?;
+    };
+}
+
+macro_rules! set_config_default {
+    ($s:ident, $id:ident, $d:expr) => {
+        $s.$id = var(&stringify!($id).to_uppercase()).unwrap_or_else(|_| $d);
+    };
+}
+
 impl Config {
     pub fn new() -> Self {
         Self::default()
@@ -36,57 +66,40 @@ impl Config {
     }
 
     pub fn init_config() -> Result<Self, Error> {
-        let fname = "config.env";
+        let fname = Path::new("config.env");
+        let home_dir =
+            dirs::home_dir().ok_or_else(|| format_err!("Cannot determine home directory"))?;
+        let config_dir = dirs::config_dir().ok_or_else(|| format_err!("No CONFIG directory"))?;
+        let default_fname = config_dir.join("diary_app_rust").join("config.env");
 
-        let home_dir = var("HOME").map_err(|e| format_err!("No HOME directory {}", e))?;
-
-        let default_fname = format!("{}/.config/diary_app_rust/config.env", home_dir);
-
-        let env_file = if Path::new(fname).exists() {
-            fname.to_string()
+        let env_file = if fname.exists() {
+            fname
         } else {
-            default_fname
+            &default_fname
         };
 
         dotenv::dotenv().ok();
 
-        if Path::new(&env_file).exists() {
-            dotenv::from_path(&env_file).ok();
-        } else if Path::new("config.env").exists() {
-            dotenv::from_filename("config.env").ok();
+        if env_file.exists() {
+            dotenv::from_path(env_file).ok();
         }
 
-        let database_url =
-            var("DATABASE_URL").map_err(|e| format_err!("DATABASE_URL must be set {}", e))?;
-        let diary_bucket = var("DIARY_BUCKET").unwrap_or_else(|_| "diary_bucket".to_string());
-        let diary_path =
-            var("DIARY_PATH").unwrap_or_else(|_| format!("{}/Dropbox/epistle", home_dir));
-        let aws_region_name = var("AWS_REGION_NAME").unwrap_or_else(|_| "us-east-1".to_string());
-        let telegram_bot_token = var("TELEGRAM_BOT_TOKEN").unwrap_or_else(|_| "".to_string());
-        let ssh_url = var("SSH_URL").ok().and_then(|s| s.parse().ok());
-        let port = var("PORT")
-            .ok()
-            .and_then(|p| p.parse().ok())
-            .unwrap_or(3042);
-        let domain = var("DOMAIN").unwrap_or_else(|_| "localhost".to_string());
-        let n_db_workers = var("N_DB_WORKERS")
-            .ok()
-            .and_then(|n| n.parse().ok())
-            .unwrap_or(2);
-        let secret_key = var("SECRET_KEY").unwrap_or_else(|_| "0123".repeat(8));
+        let mut conf = ConfigInner::default();
 
-        let conf = ConfigInner {
-            database_url,
-            diary_bucket,
+        set_config_must!(conf, database_url);
+        set_config_default!(conf, diary_bucket, "diary_bucket".to_string());
+        set_config_default!(
+            conf,
             diary_path,
-            aws_region_name,
-            telegram_bot_token,
-            ssh_url,
-            port,
-            domain,
-            n_db_workers,
-            secret_key,
-        };
+            home_dir.join("Dropbox").join("epistle").to_string_lossy().into()
+        );
+        set_config_default!(conf, aws_region_name, "us-east-1".to_string());
+        set_config_default!(conf, telegram_bot_token, "".to_string());
+        set_config_parse!(conf, ssh_url);
+        set_config_parse_default!(conf, port, 3042);
+        set_config_default!(conf, domain, "localhost".to_string());
+        set_config_parse_default!(conf, n_db_workers, 2);
+        set_config_default!(conf, secret_key, "0123".repeat(8));
 
         Ok(Self(Arc::new(conf)))
     }
