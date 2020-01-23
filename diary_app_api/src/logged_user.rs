b01b18d2ddf65a49df266c4ed776fff81d6d9ee3
Daniel Boline <ddboline@gmail.com>
2020-01-23 15:59:34 -0500

    clippy

--- diary_app_api/src/logged_user.rs
+++ diary_app_api/src/logged_user.rs
@@ -11,7 +11,7 @@ pub fn fill_from_db(pool: &PgPool) -> Result<(), Error> {
         .map(|user| LoggedUser { email: user.email })
         .collect();
 
-    if let Ok("true") = var("TESTENV").as_ref().map(|x| x.as_str()) {
+    if let Ok("true") = var("TESTENV").as_ref().map(String::as_str) {
         let user = LoggedUser {
             email: "user@test".to_string(),
         };
