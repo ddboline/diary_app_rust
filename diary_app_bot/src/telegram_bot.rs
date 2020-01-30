9e06bca28a9046673673dd8de3c6216ed9355841
Daniel Boline <ddboline@gmail.com>
2020-01-30 16:39:35 -0500

    make diary_app_bot mostly async

--- diary_app_bot/src/telegram_bot.rs
+++ diary_app_bot/src/telegram_bot.rs
@@ -1,17 +1,15 @@
-use actix_rt::System;
 use actix_threadpool::run as block;
 use anyhow::{format_err, Error};
-use crossbeam_utils::thread::{self, Scope};
 use futures::StreamExt;
 use lazy_static::lazy_static;
 use log::debug;
-use parking_lot::RwLock;
 use std::collections::HashSet;
 use std::sync::atomic::{AtomicUsize, Ordering};
-use std::thread::sleep;
-use std::time::Duration;
 use telegram_bot::types::refs::UserId;
 use telegram_bot::{Api, CanReplySendMessage, MessageKind, UpdateKind};
+use tokio::sync::RwLock;
+use tokio::time::{delay_for, timeout, Duration};
+use futures::future::join;
 
 use diary_app_lib::config::Config;
 use diary_app_lib::diary_app_interface::DiaryAppInterface;
@@ -74,18 +72,6 @@ impl FailureCount {
     }
 }
 
-fn _run_bot(dapp: DiaryAppInterface, scope: &Scope) -> Result<(), Error> {
-    let pool_ = dapp.pool.clone();
-    let userid_handle = scope.spawn(move |_| fill_telegram_user_ids(&pool_));
-    let telegram_handle = scope.spawn(move |_| telegram_worker(&dapp));
-
-    telegram_handle.join().expect("telegram thread paniced")?;
-    if userid_handle.join().is_err() {
-        panic!("Userid thread paniced, kill everything");
-    }
-    Ok(())
-}
-
 async fn bot_handler(dapp_interface: DiaryAppInterface) -> Result<(), Error> {
     let api = Api::new(&dapp_interface.config.telegram_bot_token);
     let mut stream = api.stream();
@@ -98,7 +84,7 @@ async fn bot_handler(dapp_interface: DiaryAppInterface) -> Result<(), Error> {
                 FAILURE_COUNT.check()?;
                 // Print received text message to stdout.
                 debug!("{:?}", message);
-                if TELEGRAM_USERIDS.read().contains(&message.from.id) {
+                if TELEGRAM_USERIDS.read().await.contains(&message.from.id) {
                     FAILURE_COUNT.check()?;
                     let first_word = data.split_whitespace().nth(0);
                     match first_word
@@ -111,16 +97,19 @@ async fn bot_handler(dapp_interface: DiaryAppInterface) -> Result<(), Error> {
                                 .trim_start_matches(first_word.unwrap())
                                 .trim()
                                 .to_string();
-                            OUTPUT_BUFFER.write().clear();
+                            OUTPUT_BUFFER.write().await.clear();
                             let d = dapp_interface.clone();
                             if let Ok(mut search_results) =
                                 block(move || d.search_text(&search_text)).await
                             {
                                 search_results.reverse();
-                                OUTPUT_BUFFER.write().extend_from_slice(&search_results);
+                                OUTPUT_BUFFER
+                                    .write()
+                                    .await
+                                    .extend_from_slice(&search_results);
                             }
                             FAILURE_COUNT.check()?;
-                            if let Some(entry) = OUTPUT_BUFFER.write().pop() {
+                            if let Some(entry) = OUTPUT_BUFFER.write().await.pop() {
                                 api.send(message.text_reply(entry)).await?;
                             } else {
                                 api.send(message.text_reply("...")).await?;
@@ -128,7 +117,7 @@ async fn bot_handler(dapp_interface: DiaryAppInterface) -> Result<(), Error> {
                             FAILURE_COUNT.check()?;
                         }
                         Some(":next") | Some(":n") => {
-                            if let Some(entry) = OUTPUT_BUFFER.write().pop() {
+                            if let Some(entry) = OUTPUT_BUFFER.write().await.pop() {
                                 api.send(message.text_reply(entry)).await?;
                             } else {
                                 api.send(message.text_reply("...")).await?;
@@ -183,27 +172,24 @@ async fn bot_handler(dapp_interface: DiaryAppInterface) -> Result<(), Error> {
     Ok(())
 }
 
-fn telegram_worker(dapp: &DiaryAppInterface) -> Result<(), Error> {
+async fn telegram_worker(dapp: &DiaryAppInterface) -> Result<(), Error> {
     loop {
         FAILURE_COUNT.check()?;
         let d = dapp.clone();
 
-        if System::new("diary_telegram_bot")
-            .block_on(bot_handler(d))
-            .is_ok()
-        {
-            FAILURE_COUNT.reset()?;
-        } else {
-            FAILURE_COUNT.increment()?;
+        match timeout(Duration::from_secs(3600), bot_handler(d)).await {
+            Err(_) | Ok(Ok(_)) => FAILURE_COUNT.reset()?,
+            Ok(Err(_)) => FAILURE_COUNT.increment()?,
         }
     }
 }
 
-fn fill_telegram_user_ids(pool: &PgPool) -> Result<(), Error> {
+async fn fill_telegram_user_ids(pool: PgPool) -> Result<(), Error> {
     loop {
         FAILURE_COUNT.check()?;
-        if let Ok(authorized_users) = AuthorizedUsers::get_authorized_users(pool) {
-            let mut telegram_userid_set = TELEGRAM_USERIDS.write();
+        let p = pool.clone();
+        if let Ok(authorized_users) = block(move || AuthorizedUsers::get_authorized_users(&p)).await {
+            let mut telegram_userid_set = TELEGRAM_USERIDS.write().await;
             telegram_userid_set.clear();
             for user in authorized_users {
                 if let Some(userid) = user.telegram_userid {
@@ -214,16 +200,20 @@ fn fill_telegram_user_ids(pool: &PgPool) -> Result<(), Error> {
         } else {
             FAILURE_COUNT.increment()?
         }
-        sleep(Duration::from_secs(60));
+        delay_for(Duration::from_secs(60)).await;
     }
 }
 
-pub fn run_bot() -> Result<(), Error> {
+pub async fn run_bot() -> Result<(), Error> {
     let config = Config::init_config().unwrap();
     let pool = PgPool::new(&config.database_url);
-    let dapp_interface = DiaryAppInterface::new(config, pool);
+    let dapp = DiaryAppInterface::new(config, pool);
 
-    thread::scope(|scope| _run_bot(dapp_interface, scope))
-        .map_err(|x| format_err!("{:?}", x))
-        .and_then(|r| r)
+    let pool_ = dapp.pool.clone();
+
+    let userid_handle = fill_telegram_user_ids(pool_);
+    let telegram_handle = telegram_worker(&dapp);
+
+    let (r0, r1) = join(userid_handle, telegram_handle).await;
+    r0.and_then(|_| r1)
 }
