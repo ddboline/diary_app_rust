d171f3b421d5a8a6118154b6a7e789f7f2e2b75b
Daniel Boline <ddboline@gmail.com>
2020-02-25 07:47:42 -0500

    clippy

--- diary_app_api/src/app.rs
+++ diary_app_api/src/app.rs
@@ -31,8 +31,6 @@ pub struct AppState {
 }
 
 pub async fn run_app() {
-    TRIGGER_DB_UPDATE.set();
-
     async fn _update_db(pool: PgPool) {
         let mut i = interval(time::Duration::from_secs(60));
         loop {
@@ -40,6 +38,7 @@ pub async fn run_app() {
             fill_from_db(&pool).await.unwrap_or(());
         }
     }
+    TRIGGER_DB_UPDATE.set();
 
     let config = Config::init_config().expect("Failed to load config");
     let pool = PgPool::new(&config.database_url);
