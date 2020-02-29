6cbbd25b6b9b1bc85ec81f867ff1f4d4f2a908f5
Daniel Boline <ddboline@gmail.com>
2020-02-29 14:45:36 -0500

    use rust_auth_server::static_files::login_html instead of using a local copy

--- diary_app_api/src/errors.rs
+++ diary_app_api/src/errors.rs
@@ -3,6 +3,7 @@ use actix_web::{error::ResponseError, HttpResponse};
 use anyhow::Error as AnyhowError;
 use std::fmt::Debug;
 use thiserror::Error;
+use rust_auth_server::static_files;
 
 use crate::logged_user::TRIGGER_DB_UPDATE;
 
@@ -27,13 +28,7 @@ impl ResponseError for ServiceError {
             Self::BadRequest(ref message) => HttpResponse::BadRequest().json(message),
             Self::Unauthorized => {
                 TRIGGER_DB_UPDATE.set();
-                HttpResponse::Ok()
-                    .content_type("text/html; charset=utf-8")
-                    .body(
-                        include_str!("../../templates/login.html")
-                            .replace("main.css", "/auth/main.css")
-                            .replace("main.js", "/auth/main.js"),
-                    )
+                static_files::login_html()
             }
             _ => {
                 HttpResponse::InternalServerError().json("Internal Server Error, Please try later")
