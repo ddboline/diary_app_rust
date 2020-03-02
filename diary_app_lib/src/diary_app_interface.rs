6fd6e8897bbefacde333e30318c6b82738736d88
Daniel Boline <ddboline@gmail.com>
2020-03-01 20:31:51 -0500

    stick it in, but pull it out after running once

--- diary_app_lib/src/diary_app_interface.rs
+++ diary_app_lib/src/diary_app_interface.rs
@@ -237,6 +237,9 @@ impl DiaryAppInterface {
             let local = self.local.clone();
             async move { local.import_from_local().await }
         });
+
+        self.s3.fix_s3().await?;
+
         let s3 = spawn({
             let s3 = self.s3.clone();
             async move { s3.import_from_s3().await }
