5eab92e6be3707d73b0522fb81759daeca266747
Daniel Boline <ddboline@gmail.com>
2020-10-09 14:13:22 -0400

    cleanup

--- scripts/bootstrap_db.sh
+++ scripts/bootstrap_db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PASSWORD=`head -c1000 /dev/urandom | tr -dc [:alpha:][:digit:] | head -c 16; echo ;`
-JWT_SECRET=`head -c1000 /dev/urandom | tr -dc [:alpha:][:digit:] | head -c 32; echo ;`
-SECRET_KEY=`head -c1000 /dev/urandom | tr -dc [:alpha:][:digit:] | head -c 32; echo ;`
+if [ -z "$PASSWORD" ]; then
+    PASSWORD=`head -c1000 /dev/urandom | tr -dc [:alpha:][:digit:] | head -c 16; echo ;`
+fi
 DB=diary_app_cache
 
 sudo apt-get install -y postgresql
@@ -13,6 +13,13 @@ sudo -u postgres psql -c "ALTER ROLE $USER PASSWORD '$PASSWORD' NOSUPERUSER NOCR
 sudo -u postgres createdb $DB
 
 mkdir -p ${HOME}/.config/diary_app_rust
-cat > ${HOME}/.config/sync_app_rust/config.env <<EOL
+cat > ${HOME}/.config/diary_app_rust/config.env <<EOL
 DATABASE_URL=postgresql://$USER:$PASSWORD@localhost:5432/$DB
+EOL
+
+cat > ${HOME}/.config/diary_app_rust/postgres.toml <<EOL
+[diary_app_rust]
+database_url = 'postgresql://$USER:$PASSWORD@localhost:5432/$DB'
+destination = 'file://${HOME}/setup_files/build/diary_app_rust/backup'
+tables = ['diary_entries']
 EOL
