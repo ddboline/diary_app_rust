41d0df7e99263030935089efdb2a020dbb4890d9
Daniel Boline <ddboline@gmail.com>
2024-11-21 08:44:55 -0500

    get_most_recent

--- diary_app_lib/src/models.rs
+++ diary_app_lib/src/models.rs
@@ -41,6 +41,7 @@ impl PartialEq for DiaryCache {
 pub struct AuthorizedUsers {
     pub email: StackString,
     pub telegram_userid: Option<i64>,
+    pub created_at: OffsetDateTime,
 }
 
 #[derive(FromSqlRow, Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
@@ -59,9 +60,31 @@ impl AuthorizedUsers {
     pub async fn get_authorized_users(
         pool: &PgPool,
     ) -> Result<impl Stream<Item = Result<Self, PqError>>, Error> {
-        let query = query!("SELECT * FROM authorized_users");
+        let query = query!("SELECT * FROM authorized_users WHERE deleted_at IS NULL");
         let conn = pool.get().await?;
         query.fetch_streaming(&conn).await.map_err(Into::into)
+    }
+
+    /// # Errors
+    /// Returns error if db query fails
+    pub async fn get_most_recent(
+        pool: &PgPool,
+    ) -> Result<(Option<OffsetDateTime>, Option<OffsetDateTime>), Error> {
+        #[derive(FromSqlRow)]
+        struct CreatedDeleted {
+            created_at: Option<OffsetDateTime>,
+            deleted_at: Option<OffsetDateTime>,
+        }
+
+        let query = query!(
+            "SELECT max(created_at) as created_at, max(deleted_at) as deleted_at FROM users"
+        );
+        let conn = pool.get().await?;
+        let result: Option<CreatedDeleted> = query.fetch_opt(&conn).await?;
+        match result {
+            Some(result) => Ok((result.created_at, result.deleted_at)),
+            None => Ok((None, None)),
+        }
     }
 }
 
